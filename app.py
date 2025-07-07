15c5b69de09194562b4f5da8f1ba1d560ea62b5c
0scarChavezz <oscar.chavez@unas.edu.pe>
2025-07-07 10:33:26 -0500

    Prueba test cambio ultimo test

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def home():
 
 @app.route('/api/suma')
 def suma():
-    return jsonify({"resultado": 4 + 8})
+    return jsonify({"resultado": 5 + 7})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
